b6ac823cf9115656e63c96075c7f6c95956a3349
godzspeed <godzspeed@vmgod>
2019-11-02 19:00:09 +0900

    11011900

--- src/main/java/com/example/demo/controller/CompanyController.java
+++ src/main/java/com/example/demo/controller/CompanyController.java
@@ -1,5 +1,24 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Company;
+import com.example.demo.repository.interfaces.ICompanyRepository;
+
+@RestController
 public class CompanyController {
+	
+	@Autowired
+	private ICompanyRepository repository;
+	
+	@GetMapping(path = "/company")
+	public Company getBusinessNumber(@RequestParam(required=true, defaultValue="000-00-00000")String businessNumber) throws Exception {
+		Company getbusinessNumber = repository.getBusinessNumber(businessNumber);
+		return getbusinessNumber;
+	}
+	
 
 }
--- src/main/java/com/example/demo/repository/interfaces/ICompanyRepository.java
+++ src/main/java/com/example/demo/repository/interfaces/ICompanyRepository.java
@@ -1,5 +1,9 @@
 package com.example.demo.repository.interfaces;
 
+import com.example.demo.model.Company;
+
 public interface ICompanyRepository {
+
+	Company getBusinessNumber(String businessNumber) throws Exception;
 
 }

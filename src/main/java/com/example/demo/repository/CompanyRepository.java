d50caadeb471f7e37cfd554f6c5ed2df27fe6ade
godzspeed <godzspeed@vmgod>
2019-11-02 18:34:43 +0900

    1101 1834

---
+++ src/main/java/com/example/demo/repository/CompanyRepository.java
@@ -0,0 +1,21 @@
+package com.example.demo.repository;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Company;
+import com.example.demo.repository.interfaces.ICompanyRepository;
+
+@Repository
+public class CompanyRepository implements ICompanyRepository {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	/*
+	 * public Company getBusinessNumber(String userId) throws Exception { return
+	 * sqlSession.selectOne("mapper.CompanyMapper, parameter) }
+	 */
+			
+}
--- src/main/java/com/example/demo/repository/interfaces/ICompanyRepository.java
+++ src/main/java/com/example/demo/repository/interfaces/ICompanyRepository.java
@@ -1,5 +1,5 @@
 package com.example.demo.repository.interfaces;
 
-public interface IBusinessRegistRepository {
+public interface ICompanyRepository {
 
 }

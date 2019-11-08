b47cbeb2e079eb89d3269f48abef1e512192bb53
unixut3 <dlwodusrj@naver.com>
2019-11-08 18:31:29 +0900

    [재연] 리뷰,헤어스타일 수정

--- src/main/java/com/example/demo/repository/HairStyleRepository.java
+++ src/main/java/com/example/demo/repository/HairStyleRepository.java
@@ -1,11 +1,13 @@
 package com.example.demo.repository;
 
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
 import com.example.demo.model.HairStyle;
 import com.example.demo.repository.interfaces.IHairStyleRepository;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public class HairStyleRepository implements IHairStyleRepository {
@@ -14,8 +16,31 @@ public class HairStyleRepository implements IHairStyleRepository {
 	private SqlSession sqlSession;
 	
 	@Override
+	public List<HairStyle> getHairStyleList() throws Exception{
+		return sqlSession.selectList("mapper.HairStyleMapper.getHairStyleList");
+	}
+
+
+	@Override
 	public HairStyle getHairStyle(Long styleNo) throws Exception {
 		return sqlSession.selectOne("mapper.HairStyleMapper.getHairStyle",styleNo);
+	}
+
+	@Override
+	public int insertHairStyle(com.example.demo.model.HairStyle hairStyle) throws Exception {
+		return  sqlSession.insert("mapper.HairStyleMapper.insertHairStyle");
+	}
+
+	@Override
+	public int updateHairStyle(Long styleNo) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteHairStyle(Long styleNo) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }

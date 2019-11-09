647abbbd56578ea07d01ea4bfb94165202b796d3
rusiell <outlook.com>
2019-11-09 17:38:37 +0900

    [재익] - 사업자 정보 변경하기

--- src/main/java/com/example/demo/controller/CompanyController.java
+++ src/main/java/com/example/demo/controller/CompanyController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Company;
+import com.example.demo.model.UserRegist;
 import com.example.demo.repository.interfaces.ICompanyRepository;
 
 @RestController
@@ -22,6 +23,19 @@ public class CompanyController {
 		return getbusinessNumber;
 	}
 	
+	
+		// -> 웹에서 입력한 데이터는 currentCompany(클래스 변수)에 넣고 -- Controller 에서 받는 정보
+		// -> DB에서 Company 정보를 preCompany(클래스 변수)에 담는다. -- DB에서 가져오는 정보
+		//   -> preCompancy 테이블에 넣는다.
+
+		// -> currentCompany 정보를 Company 테이블에 넣는다
+		//  ( 현재 있는 정보를 지우고)
+	public void getCompany(UserRegist user, Company com){
+		Company preCompany = null; // repository.getBusinessNumber(user.getBusinessNUmber());
+
+		CompanyUser comUser = new CompanyUser(user, com, preCom);
+	}
+
 	/*
 	 * @GetMapping(path="/company") public List<Company> companyList() throws
 	 * Exception{ return repository.companyList(); }

6ea22c6027dfb3e21c4fb834d1a72339164b01e1
Shirokuma <Shirokuma@172.16.11.202>
2019-11-09 19:40:06 +0900

    [슬] company 수정중중중중

--- src/main/java/com/example/demo/controller/CompanyController.java
+++ src/main/java/com/example/demo/controller/CompanyController.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Company;
+import com.example.demo.model.PreCompany;
 import com.example.demo.model.UserRegist;
 import com.example.demo.repository.interfaces.ICompanyRepository;
 
@@ -27,8 +27,6 @@ public class CompanyController {
 		Company getbusinessNumber = repository.getBusinessNumber(businessNumber);
 		return getbusinessNumber;
 	}
-	
-<<<<<<< HEAD
 	@GetMapping(path="/company") 
 	public List<Company> companyList() throws Exception{ 
 		return repository.companyList(); 		
@@ -43,24 +41,21 @@ public class CompanyController {
 	public int deleteCompany(@PathVariable String businessNumber) throws Exception{
 		return repository.deleteCompany(businessNumber);
 	}
-=======
-	
+
 		// -> 웹에서 입력한 데이터는 currentCompany(클래스 변수)에 넣고 -- Controller 에서 받는 정보
 		// -> DB에서 Company 정보를 preCompany(클래스 변수)에 담는다. -- DB에서 가져오는 정보
 		//   -> preCompancy 테이블에 넣는다.
 
 		// -> currentCompany 정보를 Company 테이블에 넣는다
 		//  ( 현재 있는 정보를 지우고)
-	public void getCompany(UserRegist user, Company com){
-		Company preCompany = null; // repository.getBusinessNumber(user.getBusinessNUmber());
-
+	
+	public void getCompany(UserRegist user, Company com) throws Exception{
+		//Company currentCompany = new Company(user.getBusinessNumber(),com.getCompanyName());
+		Company company =repository.getBusinessNumber(user.getBusinessNumber());
+		PreCompany preCom = new PreCompany(company.getCompanyName(), user.getUserId());
 		//CompanyUser comUser = new CompanyUser(user, com, preCom);
 	}
 
-	/*
-	 * @GetMapping(path="/company") public List<Company> companyList() throws
-	 * Exception{ return repository.companyList(); }
-	 */
->>>>>>> branch 'master' of https://github.com/vmgodxm/springboot
+	
 
 }

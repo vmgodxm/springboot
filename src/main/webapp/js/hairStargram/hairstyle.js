44db06339e87243b4692f1e17a0ffc09405b3be9
unixut3 <dlwodusrj@naver.com>
2019-11-15 17:24:02 +0900

    [재연]헤어스타일 페이지 수정

--- src/main/webapp/js/hairStargram/hairstyle.js
+++ src/main/webapp/js/hairStargram/hairstyle.js
@@ -1,4 +1,19 @@
 
 function hairStyle_body(){
-    $('#contents').load('./hairStyle/hairStyles-body.html');
+    $('#contents').load('./hair/hairStyles-body.html');
+}
+function designer(){
+    $('#btnDesigner').on('click',function(){
+        $('#contents').load('./desinger/designers.html');
+    });
+}
+function hairStyle_rev(){
+    $('#btnReservation').on('click',function(){
+        $('#contents').load('./hair/hairStyles-rev.html');
+    });
+}
+function hairStyle_done(){
+    $('#btnRev').on('click',function(){
+        $('#contents').load('./hair/hairStyles-done.html');
+    });
 }
